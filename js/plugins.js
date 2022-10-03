e35cb1b12a35be31180a407bb748940d8b1a84f6
jenneleigh <40701202+jenneleigh@users.noreply.github.com>
2022-10-03 15:33:26 -0700

    fix onboarding broken links

--- js/plugins.js
+++ js/plugins.js
@@ -53,21 +53,21 @@ $(document).ready(function () {
 
 // Can also be included with a regular script tag
 
-var Typed = new Typed(".typed", {
-	strings: [
-		"designer. developer. creative-type person.",
-		"designer. developer. caffeine addict.",
-		"designer. developer. crazy cat lady.",
-		"designer. developer. esoteric ice cream hunter.",
-		"designer. developer. sunset spotter.",
-		"designer. developer. floof admirer.",
-		"designer. developer. Animal Crossing fixer.",
-		"designer. developer. fat birb lover.",
-		"designer. developer. creative-type person.",
-	],
-	typeSpeed: 40,
-	backSpeed: 40,
-	smartBackspace: true,
-	backDelay: 200,
-	showCursor: false,
-});
+// var Typed = new Typed(".typed", {
+// 	strings: [
+// 		"designer. developer. creative-type person.",
+// 		"designer. developer. caffeine addict.",
+// 		"designer. developer. crazy cat lady.",
+// 		"designer. developer. esoteric ice cream hunter.",
+// 		"designer. developer. sunset spotter.",
+// 		"designer. developer. floof admirer.",
+// 		"designer. developer. Animal Crossing fixer.",
+// 		"designer. developer. fat birb lover.",
+// 		"designer. developer. creative-type person.",
+// 	],
+// 	typeSpeed: 40,
+// 	backSpeed: 40,
+// 	smartBackspace: true,
+// 	backDelay: 200,
+// 	showCursor: false,
+// });
